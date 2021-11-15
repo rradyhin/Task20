848b1a4e70afefff3fd6bd8df9614b550bae63b8
Radyhin Ruslan <rradyhin@gmail.com>
2021-11-15 18:39:03 +0300

    created an object

--- Main.java
+++ Main.java
@@ -1,4 +1,29 @@
 package by.overone.lesson16b;
 
+import by.overone.lesson16b.entity.User;
+import by.overone.lesson16b.service.Impl.UserServiceImpl;
+import by.overone.lesson16b.service.UserService;
+
 public class Main {
+    public static void main(String[] args) {
+
+        UserService userService = new UserServiceImpl();
+
+        User removedUser = userService.removeUserById(7);
+
+        if (removedUser.getId() > 0) {
+            System.out.println("REMOVED: "+removedUser);
+        } else {
+            System.out.println("User not found");
+        }
+
+
+        User[] users = userService.getAllUsers();
+
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
 }
