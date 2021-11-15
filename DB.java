dd9dd948135f3531776aad2f763cb631d744a82c
Radyhin Ruslan <rradyhin@gmail.com>
2021-11-15 17:55:30 +0300

    filled in database

--- DB.java
+++ DB.java
@@ -1,4 +1,21 @@
 package by.overone.lesson16b;
 
 public class DB {
+    public static String[] users;
+
+
+    static {
+        users = new String[10];
+        users[0] = "id: 1, name: Ivan, surname: Ivanov, age: 34, phoneNumber: +123456781212";
+        users[1] = "id: 2, name: Igor, surname: Iganov, age: 12, phoneNumber: +123456321212";
+        users[2] = "id: 3, name: Irina, surname: Ichkina, age: 18, phoneNumber: +123126781212";
+        users[3] = "id: 4, name: Kate, surname: Block, age: 30, phoneNumber: +12345users.add";
+        users[4] = "id: 5, name: Nick, surname: Brown, age: 17, phoneNumber: +121256781212";
+        users[5] = "id: 6, name: Garry, surname: Wilson, age: 20, phoneNumber: +123450781212";
+        users[6] = "id: 7, name: Helen, surname: Nuihelensney, age: 60, phoneNumber: +123456731212";
+        users[7] = "id: 8, name: Kirill, surname: Ivanov, age: 27, phoneNumber: +123236781212";
+        users[8] = "id: 9, name: Ivan, surname: Kirkorov, age: 15, phoneNumber: +123456561212";
+        users[9] = "id: 10, name: Kate, surname: Katovalova, age: 21, phoneNumber: +121167821212";
+
+    }
 }
