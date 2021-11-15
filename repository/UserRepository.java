d7ab512b2403130de0d84292365c3eda5a676b2e
Radyhin Ruslan <rradyhin@gmail.com>
2021-11-15 18:36:17 +0300

    added methods

--- repository/UserRepository.java
+++ repository/UserRepository.java
@@ -1,4 +1,15 @@
 package by.overone.lesson16b.repository;
 
+import by.overone.lesson16b.entity.User;
+
 public interface UserRepository {
+    User[] getAllUsers();
+
+    User getUserById(long id);
+
+    User getUserByFullName(String fullName);
+
+    User removeUserById(long id);
+
+    User addUser(User user);
 }
