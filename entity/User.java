1d511219de37a691524ab4a4bc2faa425471dd53
Radyhin Ruslan <rradyhin@gmail.com>
2021-11-15 18:16:35 +0300

    create entity, class User

--- entity/User.java
+++ entity/User.java
@@ -1,4 +1,49 @@
 package by.overone.lesson16b.entity;
 
 public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+    private String phoneNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
